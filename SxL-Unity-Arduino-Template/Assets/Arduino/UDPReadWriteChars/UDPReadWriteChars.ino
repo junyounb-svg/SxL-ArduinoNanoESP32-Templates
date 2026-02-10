65182a34b49889e9f6c1d0c898d050f2232e9abc
John Choi <johnchoi313@gmail.com>
2026-02-10 02:56:54 -0500

    Everything confirmed functional.

--- SxL-Unity-Arduino-Template/Assets/Arduino/UDPReadWriteChars/UDPReadWriteChars.ino
+++ SxL-Unity-Arduino-Template/Assets/Arduino/UDPReadWriteChars/UDPReadWriteChars.ino
@@ -6,12 +6,12 @@
 //Sample board supplier: https://www.amazon.com/HiLetgo-ESP-WROOM-32-Development-Microcontroller-Integrated/dp/B0718T232Z
 
 // Replace with your network credentials
-const char *ssid = "TP-Link_5E30";
-const char *password = "18506839";
+const char *ssid = "WIFI NETWORK NAME";//"TP-Link_5E30";
+const char *password = "WIFI NETWORK PASSWORD";//"18506839";
 
 // Set up UDP
 WiFiUDP udp;
-const char *udpAddress = "192.168.0.100";  // Target local area network IP like 192.168.1.123 
+const char *udpAddress = "192.168.0.102";  // Mac IP address 
 const int udpSenderPort = 5006;  // Port for sending data to Unity
 const int udpReceiverPort = 5005;  // Port for receiving data from Unity
 String sentMessage;
